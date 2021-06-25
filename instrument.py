9fb549b3d260bb1ab978821b855a1a97d30758d0
Quentin Stiévenart <quentin.stievenart@gmail.com>
2021-06-25 13:39:15 +0200

    Output files on disk

--- instrument.py
+++ instrument.py
@@ -20,6 +20,7 @@ class Generator(c_generator.CGenerator):
     def __init__(self, to_add):
         self.to_add = to_add
         self.added = False
+        self.added_type = 'expr'
     def visit(self, node):
         s = ''
         if not self.added and node != None and node.coord != None and self.to_add != node and self.to_add.coord.line <= node.coord.line:
@@ -27,26 +28,36 @@ class Generator(c_generator.CGenerator):
             if isinstance(node, c_ast.For):
                 # We add the instrumentation to the first line of its body
                 if isinstance(node.stmt, c_ast.Compound):
-                    node.stmt.block_items.insert(0, self.to_add)
+                    if node.stmt.block_items == None:
+                        node.stmt.block_items = [self.to_add]
+                    else:
+                        node.stmt.block_items.insert(0, self.to_add)
                 else:
                     node.stmt = c_ast.Compound([self.to_add, node.stmt])
+                self.added_type = 'for'
                 return super(Generator, self).visit(node)
             else:
                 return '%s;\n%s%s' % (super(Generator, self).visit(node), self._make_indent(), self.visit(self.to_add))
         else:
             return super(Generator, self).visit(node)
 
-def translate_to_c(ast, to_add):
+def translate_to_c(filename, ast, to_add):
     copy_ast = copy.deepcopy(ast)
     generator = Generator(to_add)
-    print(generator.visit(copy_ast))
+    transformed_source = generator.visit(copy_ast)
+    instrumented_filename = '%s_%s_%d_%s.c' % (filename[:-2], to_add.args.exprs[1].name, to_add.coord.line, generator.added_type)
+    f = open(instrumented_filename, 'w')
+    f.write(transformed_source)
+    f.close()
+
 
 if __name__ == "__main__":
     if len(sys.argv) > 1:
-        ast = parse_file(sys.argv[1], use_cpp=True)
+        filename = sys.argv[1]
+        ast = parse_file(filename, use_cpp=True)
         v = AssignmentVisitor()
         v.visit(ast)
         for to_add in v.to_add:
-            translate_to_c(ast, to_add)
+            translate_to_c(filename, ast, to_add)
     else:
         print("Please provide a filename as argument")
